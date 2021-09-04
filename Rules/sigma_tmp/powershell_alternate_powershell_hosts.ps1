d301d08a412e038f0704fe56b81175679cae88eb
Alan Smithee <nextsasasa@gmail.com>
2021-09-05 00:16:31 +0900

    adjust multi rule

--- Rules/sigma_tmp/powershell_alternate_powershell_hosts.ps1
+++ Rules/sigma_tmp/powershell_alternate_powershell_hosts.ps1
@@ -1,3 +1,6 @@
+# Get-WinEvent -LogName Microsoft-Windows-PowerShell/Operational | where {(($_.ID -eq "4103" -and $_.message -match "ContextInfo.*.*") -and  -not ($_.message -match "ContextInfo.*powershell.exe" -or $_.message -match "Message.*powershell.exe")) } | select TimeCreated,Id,RecordId,ProcessId,MachineName,Message
+# Get-WinEvent -LogName Windows PowerShell | where {(($_.ID -eq "400" -and $_.message -match "ContextInfo.*.*") -and  -not ($_.message -match "ContextInfo.*powershell.exe" -or $_.message -match "Message.*powershell.exe")) } | select TimeCreated,Id,RecordId,ProcessId,MachineName,Message
+
 
 function Add-Rule {
     param (
@@ -12,8 +15,8 @@ function Add-Rule {
                 $event
             )
             
-            $result = $event |  where {(($_.ID -eq "4103" -and $_.message -match "ContextInfo.*.*") -and -not ($_.message -match "ContextInfo.*powershell.exe" -or $_.message -match "Message.*powershell.exe")) } | select TimeCreated,Id,RecordId,ProcessId,MachineName,Message Get-WinEvent -LogName Windows PowerShell | where {(($_.ID -eq "400" -and $_.message -match "ContextInfo.*.*") -and -not ($_.message -match "ContextInfo.*powershell.exe" -or $_.message -match "Message.*powershell.exe")) } | select TimeCreated,Id,RecordId,ProcessId,MachineName,Message;
-            if ($result.Count -ne 0) {
+            $result = $event |  where { (($_.ID -eq "4103" -and $_.message -match "ContextInfo.*.*") -and -not ($_.message -match "ContextInfo.*powershell.exe" -or $_.message -match "Message.*powershell.exe")) } | select TimeCreated, Id, RecordId, ProcessId, MachineName, Message; $result2 = $event | where { (($_.ID -eq "400" -and $_.message -match "ContextInfo.*.*") -and -not ($_.message -match "ContextInfo.*powershell.exe" -or $_.message -match "Message.*powershell.exe")) } | select TimeCreated, Id, RecordId, ProcessId, MachineName, Message;
+            if (($result.Count -ne 0) -or $result2.Count -ne 0) {
                 Write-Host
                 Write-Host "Detected! RuleName:$ruleName"  
                 Write-Host
