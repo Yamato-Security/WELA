9ac5fb2e030bfc0bd110cd7e76c9040cab7a8c7f
Alan Smithee <nextsasasa@gmail.com>
2021-09-04 20:36:12 +0900

    fix add-rule template & erase debug print #28

--- Rules/SIGMA/win_susp_failed_logons_single_source_ntlm.ps1
+++ Rules/SIGMA/win_susp_failed_logons_single_source_ntlm.ps1
@@ -4,17 +4,17 @@ function Add-Rule {
     param (
         [bool] $isLiveAnalysis
     )
+    $ruleName = "win_susp_failed_logons_single_source_ntlm";
+    $detectedMessage = "Detects failed logins with multiple valid domain accounts from a single source system using the NTLM protocol."
+
     $detectRule = {
-        $ruleName = "win_susp_failed_logons_single_source_ntlm";
-        $detectedMessage = "Detects failed logins with multiple valid domain accounts from a single source system using the NTLM protocol."
         function Search-DetectableEvents {
             param (
                 $event
             )
             
             $result = $event | where { (($_.ID -eq "4776" -and $_.message -match "Status.*.*0xC000006A") -and -not ($_.message -match "TargetUserName.*.*$")) }  | select Workstation, TargetUserName | group Workstation | foreach { [PSCustomObject]@{'Workstation' = $_.name; 'Count' = ($_.group.TargetUserName | sort -u).count } }  | sort count -desc | where { $_.count -gt 10 };
-            Write-Host $result;
-            if $result.Count -ne 0 {
+            if ($result.Count -ne 0) {
                 Write-Host
                 Write-Host "Detected! RuleName:$ruleName"  
                 Write-Host
@@ -24,5 +24,5 @@ function Add-Rule {
         };
         Search-DetectableEvents $args[0];
     };
-    $Global:ruleStack.Add('win_susp_failed_logons_single_source_ntlm', $detectRule);
+    $Global:ruleStack.Add($ruleName, $detectRule);
 }
