d788714313b67f3975cb1773e80e1ef0e5d605ca
Alan Smithee <nextsasasa@gmail.com>
2021-09-06 08:28:34 +0900

    fixed lacked if case

--- Rules/SIGMA/process_creation/win_hktl_createminidump.ps1
+++ Rules/SIGMA/process_creation/win_hktl_createminidump.ps1
@@ -17,7 +17,7 @@ function Add-Rule {
             $result = $event | where { (($_.ID -eq "1") -and ($_.message -match "Image.*.*\\CreateMiniDump.exe.*" -or $_.message -match "Imphash.*4a07f944a83e8a7c2525efa35dd30e2f")) } | select TimeCreated, Id, RecordId, ProcessId, MachineName, Message
             $result2 = $event | where { ($_.ID -eq "11" -and $_.message -match "TargetFilename.*.*\\lsass.dmp") } | select TimeCreated, Id, RecordId, ProcessId, MachineName, Message;
             
-            if ($result.Count -ne 0) {
+            if (($result.Count -ne 0) -or ($result2 -ne 0)) {
                 Write-Host
                 Write-Host "Detected! RuleName:$ruleName"  
                 Write-Host
