ac18adc3ffc6e1b0ad9c83d4ee4076f7901844c1
Alan Smithee <nextsasasa@gmail.com>
2021-09-15 01:04:42 +0900

    add DeepBlueCLI Rule(4672-AdminAccountAccessAllAlert)#30

---
+++ Rules/WELA-Rules/Security/4672-AdminAccountAccessAllAlerts.ps1
@@ -0,0 +1,33 @@
+
+function Add-Rule {
+    $ruleName = "4672-AdminAccountAccessAllAlerts";
+    $detectedMessage = "Logon with SeDebugPrivilege (admin access)`nSpecial privileges assgned to new logons on DeepBlueCLI Rule";
+
+    $detectRule = {
+        function Search-DetectableEvents {
+            param (
+                $event
+            )
+            $eventXML = [xml]$event.ToXml();
+            $username = $eventXML.Event.EventData.Data[1]."#text"
+            $domain = $eventXML.Event.EventData.Data[2]."#text"
+            $securityid = $eventXML.Event.EventData.Data[3]."#text"
+            $privileges = $eventXML.Event.EventData.Data[4]."#text"
+            if ($privileges -Match "SeDebugPrivilege") {
+                $result = "Username: $username`n"
+                $result += "Domain: $domain`n"
+                $result += "User SID: $securityid`n"
+                $result += "Privileges: $privileges"
+                if ($result.Count -ne 0) {
+                    Write-Host
+                    Write-Host "Detected! RuleName:$ruleName";
+                    Write-Host $detectedMessage;
+                    Write-Host $result
+                }
+            }
+            
+        };
+        Search-DetectableEvents $args[0];
+    };
+    $Global:ruleStack.Add($ruleName, $detectRule);
+}
