a59e1a187be270c575b56eed47c90c43076c4dc6
Alan Smithee <nextsasasa@gmail.com>
2021-09-19 04:00:21 +0900

    filename changed and add DeepBlueCLI Rule(4104 PowerShell) #30

---
+++ Rules/WELA-Rules/Sysmon/1-ProcessCreation.ps1
@@ -0,0 +1,29 @@
+﻿
+function Add-Rule {
+    $ruleName = "1-ProcessCreation";
+    $detectedMessage = "detected Sysmon process creation on DeepBlueCLI Rule";
+
+    $detectRule = {
+        function Search-DetectableEvents {
+            param (
+                $event
+            )
+            $target = $event | where { $_.ID -eq 1 -and $_.ProviderName -eq "Microsoft-Windows-Sysmon/Operational" }
+
+            foreach ($record in $target) {
+                $eventXML = [xml] $record.ToXml()
+                $creator = $eventXML.Event.EventData.Data[14]."#text"
+                $commandline = $eventXML.Event.EventData.Data[4]."#text"
+                if ($commandline) {
+                    $result = Check-Command -EventID 1 -creator $creator
+                    Write-Host
+                    Write-Host "Detected! RuleName:$ruleName";
+                    Write-Host $detectedMessage;
+                    Write-host $result
+                }
+            }
+        };
+        Search-DetectableEvents $args[0];
+    };
+    $Global:ruleStack.Add($ruleName, $detectRule);
+}
