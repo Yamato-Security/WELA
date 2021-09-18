8f90671eedf20675f03924586146ce06d2ff846d
Alan Smithee <nextsasasa@gmail.com>
2021-09-19 03:18:44 +0900

    add DeepBlueCLI Rule(104 System) #30

---
+++ Rules/WELA-Rules/System/104-SystemLogClear.ps1
@@ -0,0 +1,27 @@
+﻿# Get-WinEvent -LogName Security  where {($_.ID -eq "4688" | select TimeCreated,Id,RecordId,ProcessId,MachineName,Message
+
+function Add-Rule {
+    $ruleName = "104-SystemLogClear";
+    $detectedMessage = "detected system log cleared on DeepBlueCLI Rule";
+
+    $detectRule = {
+        function Search-DetectableEvents {
+            param (
+                $event
+            )
+            $target = $event | where { $_.ID -eq 104 -and $_.ProviderName -eq "System" }
+
+            if ($target) {
+                Write-Host
+                Write-Host "Detected! RuleName:$ruleName";
+                Write-Host $detectedMessage;
+            }
+            foreach ($record in $target) {
+                $result = $record.message
+                Write-hoss $result
+            }
+        };
+        Search-DetectableEvents $args[0];
+    };
+    $Global:ruleStack.Add($ruleName, $detectRule);
+}
