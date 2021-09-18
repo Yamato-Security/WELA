9d283bc968179f9b69d9c6403453b300d44f57bd
Alan Smithee <nextsasasa@gmail.com>
2021-09-19 03:42:51 +0900

    add DeepBlueCLI Rule(8003 Applocker) #30

---
+++ Rules/WELA-Rules/Applocker/8003-ApplockerWarning.ps1
@@ -0,0 +1,26 @@
+﻿
+function Add-Rule {
+    $ruleName = "8003-ApplockerWarning";
+    $detectedMessage = "detected Applocker warningon DeepBlueCLI Rule";
+
+    $detectRule = {
+        function Search-DetectableEvents {
+            param (
+                $event
+            )
+            $target = $event | where { $_.ID -eq 8003 -and $_.ProviderName -eq "Microsoft-Windows-AppLocker/EXE and DLL" }
+
+            if ($target) {
+                Write-Host
+                Write-Host "Detected! RuleName:$ruleName";
+                Write-Host $detectedMessage;
+            }
+            foreach ($record in $target) {
+                $result = $record.message
+                Write-host $result
+            }
+        };
+        Search-DetectableEvents $args[0];
+    };
+    $Global:ruleStack.Add($ruleName, $detectRule);
+}
