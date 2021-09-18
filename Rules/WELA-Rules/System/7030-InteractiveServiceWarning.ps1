6a9ff9b8a8f10f470dbfb0753b9a230a12aee60a
Alan Smithee <nextsasasa@gmail.com>
2021-09-19 03:18:59 +0900

    add DeepBlueCLI Rule(7030 System) #30

---
+++ Rules/WELA-Rules/System/7030-InteractiveServiceWarning.ps1
@@ -0,0 +1,32 @@
+﻿# Get-WinEvent -LogName Security  where {($_.ID -eq "4688" | select TimeCreated,Id,RecordId,ProcessId,MachineName,Message
+
+function Add-Rule {
+    $ruleName = "7030-InteractiveServiceWarning";
+    $detectedMessage = "detected Interactive service warning on DeepBlueCLI Rule";
+
+    $detectRule = {
+        function Search-DetectableEvents {
+            param (
+                $event
+            )
+            $target = $event | where { $_.ID -eq 7030 -and $_.ProviderName -eq "System" }
+
+            if ($target) {
+                Write-Host
+                Write-Host "Detected! RuleName:$ruleName";
+                Write-Host $detectedMessage;
+            }
+            foreach ($record in $target) {
+                $eventXML = [xml]$record.ToXml();
+                $servicename = $eventXML.Event.EventData.Data."#text"
+                $result = "Service name: $servicename`n"
+                $result += "Malware (and some third party software) trigger this warning"
+                # Check for suspicious service name
+                $result += (Check-Regex $servicename 1)
+                Write-host $result
+            }
+        };
+        Search-DetectableEvents $args[0];
+    };
+    $Global:ruleStack.Add($ruleName, $detectRule);
+}
