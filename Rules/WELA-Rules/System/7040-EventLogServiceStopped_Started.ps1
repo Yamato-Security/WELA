e05ee5eb79c9dedf81b1119993eca645fe43fa1c
Alan Smithee <nextsasasa@gmail.com>
2021-09-19 03:19:45 +0900

    add DeepBlueCLI Rule(7040 System) #30

---
+++ Rules/WELA-Rules/System/7040-EventLogServiceStopped_Started.ps1
@@ -0,0 +1,37 @@
+﻿# Get-WinEvent -LogName Security  where {($_.ID -eq "4688" | select TimeCreated,Id,RecordId,ProcessId,MachineName,Message
+
+function Add-Rule {
+    $ruleName = "7040-EventLogServiceStopped/Started";
+    $detectedMessage = "detected event log serice stopped/started on DeepBlueCLI Rule";
+
+    $detectRule = {
+        function Search-DetectableEvents {
+            param (
+                $event
+            )
+            $target = $event | where { $_.ID -eq 7040 -and $_.ProviderName -eq "System" }
+
+            foreach ($record in $target) {
+                $eventXML = [xml]$record.ToXml();
+                $servicename = $eventXML.Event.EventData.Data[0]."#text"
+                $action = $eventXML.Event.EventData.Data[1]."#text"
+                if ($servicename -ccontains "Windows Event Log") {
+                    $result = "Service name: $servicename`n"
+                    $result += $text
+                    if ($action -eq "disabled") {
+                        $result += "Selective event log manipulation may follow this event."
+                    }
+                    elseIf ($action -eq "auto start") {
+                        $result += "Selective event log manipulation may precede this event."
+                    }
+                    Write-Host
+                    Write-Host "Detected! RuleName:$ruleName";
+                    Write-Host $detectedMessage;
+                    Write-host $result
+                }
+            }
+        };
+        Search-DetectableEvents $args[0];
+    };
+    $Global:ruleStack.Add($ruleName, $detectRule);
+}
