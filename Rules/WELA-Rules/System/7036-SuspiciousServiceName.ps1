9a6f466d9f0d2e96982ce7eb3f6aa79e3d655563
Alan Smithee <nextsasasa@gmail.com>
2021-09-19 03:19:36 +0900

    add DeepBlueCLI Rule(7036 System) #30

---
+++ Rules/WELA-Rules/System/7036-SuspiciousServiceName.ps1
@@ -0,0 +1,32 @@
+﻿# Get-WinEvent -LogName Security  where {($_.ID -eq "4688" | select TimeCreated,Id,RecordId,ProcessId,MachineName,Message
+
+function Add-Rule {
+    $ruleName = "7036-SuspiciousServiceName";
+    $detectedMessage = "detected Suspicious Service on DeepBlueCLI Rule";
+
+    $detectRule = {
+        function Search-DetectableEvents {
+            param (
+                $event
+            )
+            $target = $event | where { $_.ID -eq 7036 -and $_.ProviderName -eq "System" }
+
+            foreach ($record in $target) {
+                $eventXML = [xml]$record.ToXml();
+                $servicename = $eventXML.Event.EventData.Data[0]."#text"
+                $text = (Check-Regex $servicename 1)
+                if ($text) {
+                    $result = "Service name: $servicename`n"
+                    $result += $text
+                    
+                    Write-Host
+                    Write-Host "Detected! RuleName:$ruleName";
+                    Write-Host $detectedMessage;
+                    Write-Output $result
+                }
+            }
+        };
+        Search-DetectableEvents $args[0];
+    };
+    $Global:ruleStack.Add($ruleName, $detectRule);
+}
