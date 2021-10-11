23495d03c6d4c7a99d6f2e84906ebaff734bd616
Alan Smithee <nextsasasa@gmail.com>
2021-10-11 22:09:17 +0900

    fix double quote lack error

--- Rules/SIGMA/registry_event/sysmon_uac_bypass_sdclt.ps1
+++ Rules/SIGMA/registry_event/sysmon_uac_bypass_sdclt.ps1
@@ -14,21 +14,22 @@ unasmmandisolatedCommand"
             )
             
             $ruleName = "sysmon_uac_bypass_sdclt";
-            $detectedMessage = "Detects changes to HKCU:SoftwareClassesexefileshell
-                $result = $event |  where { ($_.ID -eq "12" -or $_.ID -eq "13" -or $_.ID -eq "14") -and $_.message -match "TargetObject.*HKU\\.*" -and $_.message -match "TargetObject.*.*_Classes\\exefile\\shell\\runas\\command\\isolatedCommand" } | select TimeCreated, Id, RecordId, ProcessId, MachineName, Message;
+            $detectedMessage = "Detects changes to HKCU:SoftwareClassesexefileshell"
+            $result = $event |  where { ($_.ID -eq "12" -or $_.ID -eq "13" -or $_.ID -eq "14") -and $_.message -match "TargetObject.*HKU\\.*" -and $_.message -match "TargetObject.*.*_Classes\\exefile\\shell\\runas\\command\\isolatedCommand" } | select TimeCreated, Id, RecordId, ProcessId, MachineName, Message;
             if ($result.Count -ne 0) {
                 Write-Host
                 Write-Host "Detected! RuleName:$ruleName";
                 Write-Host $detectedMessage;
                 Write-Host $result;
-Write-Host
+                Write-Host
             }
         };
         . Search-DetectableEvents $args;
     };
-    if(! $ruleStack[$ruleName]) {
+    if (! $ruleStack[$ruleName]) {
         $ruleStack.Add($ruleName, $detectRule);
-    } else {
-       Write-Host "Rule Import Error" -Foreground Yellow;
+    }
+    else {
+        Write-Host "Rule Import Error" -Foreground Yellow;
     }
 }
