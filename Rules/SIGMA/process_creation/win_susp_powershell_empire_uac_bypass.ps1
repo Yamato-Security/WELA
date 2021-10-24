f0703d5f7edfdc72086de5e6c2355cdbb9e97ccd
Alan Smithee <nextsasasa@gmail.com>
2021-10-24 22:03:11 +0900

    fixed not exist registory value error

--- Rules/SIGMA/process_creation/win_susp_powershell_empire_uac_bypass.ps1
+++ Rules/SIGMA/process_creation/win_susp_powershell_empire_uac_bypass.ps1
@@ -12,11 +12,16 @@ function Add-Rule {
             
             $ruleName = "win_susp_powershell_empire_uac_bypass";
             $detectedMessage = "Detects some Empire PowerShell UAC bypass methods";
-            $result = $event |  where { ($_.ID -eq "1" -and ($_.message -match "CommandLine.*.* -NoP -NonI -w Hidden -c $x=$((gp HKCU:Software\\Microsoft\\Windows Update).Update).*" -or $_.message -match "CommandLine.*.* -NoP -NonI -c $x=$((gp HKCU:Software\\Microsoft\\Windows Update).Update);.*")) } | select TimeCreated, Id, RecordId, ProcessId, MachineName, Message;
+            $updateregistory = Get-ItemProperty "HKCU:Software\\Microsoft\\Windows Update" -ErrorAction SilentlyContinue
+            # if registory data is not exist, Getada
+            if (!$updateregistory) {
+                return
+            }
+            $result = $event |  where { ($_.ID -eq "1" -and ($_.message -match "CommandLine.*.* -NoP -NonI -w Hidden -c $x=$($updateregistory.Update).*" -or $_.message -match "CommandLine.*.* -NoP -NonI -c $x=$($updateregistory.Update);.*")) } | select TimeCreated, Id, RecordId, ProcessId, MachineName, Message;
             if ($result -and $result.Count -ne 0) {
                 Write-Output ""; 
                 Write-Output "Detected! RuleName:$ruleName";
-                Write-Output $detectedMesssage;
+                Write-Output $detectedMessage;
                 Write-Output $result;
                 Write-Output ""; 
             }
