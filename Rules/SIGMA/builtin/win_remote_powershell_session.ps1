25bca584f301b2ef7a76eb3e2ac01e0dcb599c38
Alan Smithee <nextsasasa@gmail.com>
2021-11-02 20:31:48 +0900

    fixed null check

--- Rules/SIGMA/builtin/win_remote_powershell_session.ps1
+++ Rules/SIGMA/builtin/win_remote_powershell_session.ps1
@@ -13,7 +13,7 @@ function Add-Rule {
             $ruleName = "win_remote_powershell_session";
             $detectedMessage = "Detects basic PowerShell Remoting (WinRM) by monitoring for network inbound connections to ports 5985 OR 5986";
             $result = $event |  where { ($_.ID -eq "5156" -and ($_.message -match "5985" -or $_.message -match "5986") -and $_.message -match "LayerRTID.*44") } | select TimeCreated, Id, RecordId, ProcessId, MachineName, Message;
-            if ($result -and $result.Count -ne 0) {
+            if ($null -ne $result -and $result.Count -ne 0) {
                 Write-Output ""; 
                 Write-Output "Detected! RuleName:$ruleName";
                 Write-Output $detectedMessage;
