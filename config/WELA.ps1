d13026210836a44056176c882d00230849ef32a1
fukusuket <41001169+fukusuket@users.noreply.github.com>
2025-03-12 00:38:47 +0900

    update

--- config/WELA.ps1
+++ config/WELA.ps1
@@ -3,6 +3,7 @@ $outputFilePath = "auditpol_output.txt"
 Start-Process -FilePath "cmd.exe" -ArgumentList "/c chcp 437 & auditpol /get /category:* /r > $outputFilePath" -NoNewWindow -Wait
 $auditpolOutput = Get-Content -Path $outputFilePath -Raw
 $filteredOutput = $auditpolOutput | Select-String -NotMatch "No Auditing"
+Write-Host $filteredOutput
 $extractedStrings = [System.Collections.Generic.HashSet[string]]::new()
 $filteredOutput | ForEach-Object {
     if ($_ -match '{(.*?)}') {
