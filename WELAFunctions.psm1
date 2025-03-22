4ed4df8c25b5e26ab3cb96ff94fb06fe78b27fb9
fukusuket <41001169+fukusuket@users.noreply.github.com>
2025-03-22 15:27:48 +0900

    informational -> info

--- WELAFunctions.psm1
+++ WELAFunctions.psm1
@@ -194,7 +194,8 @@ function ShowRulesCountsByLevel {
     Write-Host -NoNewline " - "
     $usableRate | Sort-Object { $levelColorMap.Keys.IndexOf($_.Level) } | ForEach-Object {
         $color = $levelColorMap[$_.Level]
-        Write-Host -NoNewline "$($_.Level): $($_.UsableCount) / $($_.TotalCount) ($($_.Percentage)%)" -ForegroundColor $color
+        $level = if ($_.Level -eq "informational") { "info" } else { $_.Level }
+        Write-Host -NoNewline "$($level): $($_.UsableCount) / $($_.TotalCount) ($($_.Percentage)%)" -ForegroundColor $color
         if ($i -lt $usableRate.Count - 1)
         {
             Write-Host -NoNewline ", "
